47cf51fe23c76d94c6244f13e4e78155bbccee85
Oleh <oleh.j@ukr.net>
2021-05-18 15:00:04 +0300

    start of the project

---
+++ app.js
@@ -0,0 +1,42 @@
+
+let tab1 = document.querySelector(".main-tabpost-tab-first")
+let tab2 = document.querySelector(".main-tabpost-tab-second")
+let tabpost1 = document.querySelector(".main-tabpost-item-first")
+let tabpost2 = document.querySelector(".main-tabpost-item-second")
+let testkey = document.querySelector(".main-search-input")
+
+window.addEventListener('click', function (e){
+    if(e.target === tab1){
+        tabpost2.classList.remove("active-tab")
+        tabpost1.classList.add("active-tab")
+        tab2.classList.remove("active")
+        tab1.classList.add("active")
+
+    } if (e.target === tab2) {
+        tabpost1.classList.remove("active-tab")
+        tabpost2.classList.add("active-tab")
+        tab1.classList.remove("active")
+        tab2.classList.add("active")
+    }
+    }) 
+
+
+let footerPlus = document.querySelector(".footer-menu-item-plus")
+let footerPlusSecond = document.querySelector(".plus-second")
+let footerSubmenu = document.querySelector(".footer-submenu")
+let footerPlusFirst = document.querySelector(".plus-first")
+
+
+
+window.addEventListener('click', function (e){
+    if(e.target === footerPlus ||e.target === footerPlusFirst ||e.target === footerPlusSecond){    
+        if(footerPlusSecond.classList.contains("clouse")) {
+            footerPlusSecond.classList.remove("clouse")
+            footerSubmenu.classList.remove("active-plus")
+        } else {
+            footerPlusSecond.classList.add("clouse")
+            footerSubmenu.classList.add("active-plus")
+        }
+        
+    }
+})
